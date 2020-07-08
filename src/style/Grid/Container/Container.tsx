7a0150b31af9c4dfdd1aaeef8bf1788f8ac160fd
seonk94 <seonk94@naver.com>
2020-07-08 22:19:16 +0900

    fix: breakpoint media set

--- src/style/Grid/Container/Container.tsx
+++ src/style/Grid/Container/Container.tsx
@@ -4,10 +4,13 @@ import { FC } from 'react';
 
 import styled from '@emotion/styled';
 import { ContainerProps } from './types';
+import { BREAKPOINTS } from '../../../constants';
+import { media } from '../../media';
 
 const baseStyle = () => css`
     label: container;
     width: 100%;
+    max-width: 100%;
 
     margin-right: auto;
     margin-left: auto;
@@ -23,6 +26,16 @@ const BaseContainer: FC<ContainerProps> = ({
         </div>
     );
 
-export default styled(BaseContainer)((props) => (
-    baseStyle
-))
+const fluidStyle = ({ fluid }: { fluid: boolean }) =>
+    !fluid &&
+    BREAKPOINTS.map(
+        (breakpoint) => css`
+            ${media(breakpoint)} {
+
+            }
+        `
+    )
+export default styled(BaseContainer)(
+    baseStyle,
+    fluidStyle
+)
