0b9c92323a74daf9e97ff0bcaa0ddfbfedf0c20b
sk <seonk94@naver.com>
2020-11-28 18:48:34 +0900

    feat: Appbar UI init

---
+++ src/components/common/Appbar/index.tsx
@@ -0,0 +1,37 @@
+import { AppBar, IconButton, Menu, MenuItem, Toolbar } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
+import React, { useState } from 'react';
+
+function Appbar() {
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <div style={{ flexGrow : 1}}></div>
+        <IconButton onClick={handleMenu}>
+          <AccountCircle/>            
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Appbar;
