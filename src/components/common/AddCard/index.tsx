6c0237ee0dbb20c8596c50c7d808ba520b5595cc
sk <seonk94@naver.com>
2020-11-30 20:35:05 +0900

    fix: AddCard css

--- src/components/common/AddCard/index.tsx
+++ src/components/common/AddCard/index.tsx
@@ -6,15 +6,9 @@ import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 const useStyles = makeStyles({
   root : {
     display : 'flex',
-    maxWidth : 400,
-    minWidth : 320,
-    maxHeight : 400,
-    minHeight : 320,
-    width : '25vw',
-    height : '25vw',
     alignItems : 'center',
     justifyContent : 'center',
-    margin : 'auto'
+    margin : '12px auto'
   },
   button : {
     width : '100%',
@@ -25,11 +19,11 @@ function AddCard() {
   const classes = useStyles();
 
   return (
-    <Paper variant="outlined" className={classes.root}>
+    <Card variant="outlined" className={classes.root}>
       <Button className={classes.button}>
         <AddCircleOutlineIcon/>
       </Button>
-    </Paper>
+    </Card>
   );
 }
 
