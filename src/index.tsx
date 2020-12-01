84698902fe08f7a6fbae13e49b0eb09fe0e64a7b
sk <seonk94@naver.com>
2020-12-01 21:39:49 +0900

    feat: Theme Color palette

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Root from './router';
 import './index.css';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './style/MaterialUiTheme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/style/MaterialUiTheme.ts
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette : {
+    primary : {
+      main : '#212121'
+    },
+    secondary : {
+      main : '#29b6f6'
+    }
+  }
+});
+export default theme;
