9c356697eac999af0663f0d609a04de5d88ae034
sk <seonk94@naver.com>
2020-12-05 22:36:26 +0900

    feat: Add Login page at Router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Appbar from 'src/components/common/Appbar';
+import login from 'src/page/login';
 import main from 'src/page/main';
 
 
@@ -10,8 +11,10 @@ function Root() {
       <Appbar />
       <Switch>
         <Route exact path="/" component={main} />
+        <Route exact path="/login" component={login} />
         <Redirect path="*" to="/" />
       </Switch>
+      
     </BrowserRouter>
   );
 }
