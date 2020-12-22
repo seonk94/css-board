8ec1a536d157738fe668631316617967fd05d22c
sk <seonk94@naver.com>
2020-12-22 20:59:09 +0900

    fix: MuiPickersUtilsProvider  DatePicker 안으로 이동

--- src/components/common/DatePickerBox.tsx
+++ src/components/common/DatePickerBox.tsx
@@ -2,7 +2,8 @@ import { Box } from '@material-ui/core';
 import { DatePicker } from '@material-ui/pickers';
 import moment from 'moment';
 import React, { Dispatch, SetStateAction } from 'react';
-
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 interface Props {
   date : moment.Moment,
   onChange : Dispatch<SetStateAction<moment.Moment>>,
@@ -11,18 +12,21 @@ interface Props {
 
 function DatePickerBox({ date, onChange, classes } : Props) {
   return (
-    <Box>
-      <DatePicker
-        value={date}
-        className={classes}
-        variant="inline"
-        format="YYYY-MM-DD"
-        inputVariant="outlined"
-        label="Date"
-        autoOk
-        onChange={date => onChange(date as moment.Moment)}
-      />
-    </Box>
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+
+      <Box>
+        <DatePicker
+          value={date}
+          className={classes}
+          variant="inline"
+          format="YYYY-MM-DD"
+          inputVariant="outlined"
+          label="Date"
+          autoOk
+          onChange={date => onChange(date as moment.Moment)}
+        />
+      </Box>
+    </MuiPickersUtilsProvider>
   );
 }
 
